f829e9caca8f6475d37407d86c9f5896048468a9
Priyansh Sahu <priyansh.sahu25@gmail.com>
2025-01-27 18:30:56 +0000

    API CALL

--- One/main.go
+++ One/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+// USING API TO FETCH DATA AND MARSHAL AND UNMARSHAL
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
 func main() {
-fmt.Println("Hello")	
+res,err:=http.Get("https://official-joke-api.appspot.com/random_ten")
+if err!=nil || res.StatusCode!= http.StatusOK  {
+	fmt.Println("Error1:",err)
+}
+defer res.Body.Close()
+body,readErr:=ioutil.ReadAll(res.Body)
+if readErr!=nil{
+	fmt.Println("Error2",readErr)
+}else{
+	fmt.Println("Body :",string(body))
+}
+
 }
