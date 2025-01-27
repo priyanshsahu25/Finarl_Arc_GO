80174a94c2f0d5e461ab328276bad8738f387560
Priyansh Sahu <priyansh.sahu25@gmail.com>
2025-01-27 19:14:59 +0000

    JSON DISPLAY

---
+++ One/API_CALL_JSON_DISPLAY/main.go
@@ -0,0 +1,36 @@
+package main
+
+// USING API TO FETCH DATA AND MARSHAL AND UNMARSHAL
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type  Joke struct {
+Id int `json:"id"`
+Setup string `json:"setup"`
+Punchline string `json:"punchline"`
+}
+
+
+
+func main() {
+res,err:=http.Get("https://official-joke-api.appspot.com/random_ten")
+if err!=nil || res.StatusCode!= http.StatusOK  {
+	fmt.Println("Error1:",err)
+}
+defer res.Body.Close()
+
+var jokes []Joke
+
+DecodeErr:=json.NewDecoder(res.Body).Decode(&jokes)
+if DecodeErr!= nil{
+	fmt.Println("Error3",DecodeErr)
+}
+
+for _,val:=range jokes{
+	fmt.Println(val.Punchline)
+}
+
+}
